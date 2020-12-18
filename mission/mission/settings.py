ca0dba8d082858f400f1eeb8d7a24df306fea403
jeffwrh <jeffwrh@gmail.com>
2020-12-18 15:47:16 +0100

    sauvegarde

--- mission/mission/settings.py
+++ mission/mission/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vl@q%ev8ja!pm11!l&6)9nkdla@w1bjx$7g=xn-^f$%kx)2^sp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gpo.apps.GpoConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'europe/paris'
 
 USE_I18N = True
 
